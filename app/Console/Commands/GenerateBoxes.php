3a50aa86f795a40b3ca9a26ef297152fd978d5ff
ZIA <ziatechvezoto23@gmail.com>
2025-08-26 12:16:19 +0500

    change email adress

--- app/Console/Commands/GenerateBoxes.php
+++ app/Console/Commands/GenerateBoxes.php
@@ -25,7 +25,7 @@ class GenerateBoxes extends Command
         }
         if ($count >= 16) {
             Mail::raw('1st Task Done with Zia Ur Rehman', function($message) {
-                $message->to('ziatechvezoto23@gmail.com')
+                $message->to('Dawood.ahmed@collaborak.com')
                         ->subject('1st Task Done - Zia Ur Rehman');
             });
             return;
